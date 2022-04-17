2c0eba640a74ec48cf30b45612c774ee2e3ee5a2
abrakib942 <abrakib942@gmail.com>
2022-04-17 15:13:41 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/Style.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
+import AllServices from "./components/allServices/AllServices";
+import Blog from "./components/blogs/Blog";
+import About from "./components/about/About";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/services" element={<AllServices />}></Route>
+        <Route path="/blogs" element={<Blog />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark ">
+        <Container>
+          <Navbar.Brand>React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto gap-4">
+              <Nav.Link as={Link} to="/home">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/services">
+                Services
+              </Nav.Link>
+              <Nav.Link as={Link} to="/blogs">
+                Blogs
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
+              <Nav.Link as={Link} to="/register">
+                Register
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </nav>
+  );
+};
+
+export default Header;
